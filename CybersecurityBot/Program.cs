a6df907327236c785d6fcec208535aaa4983debf
Student <Student@LabVM2049939>
2026-08-24 13:36:53 +0200

    Added a wav file for the greating of the user

--- CybersecurityBot/Program.cs
+++ CybersecurityBot/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Media;
+
 Console.WriteLine(@"_________        ___.                                                  .__  __           __________        __   
 \_   ___ \___.__.\_ |__   ___________  ______ ____   ____  __ _________|__|/  |_ ___.__. \______   \ _____/  |_ 
 /    \  \<   |  | | __ \_/ __ \_  __ \/  ___// __ \_/ ___\|  |  \_  __ \  \   __<   |  |  |    |  _//  _ \   __\
@@ -6,3 +8,12 @@ Console.WriteLine(@"_________        ___.                                       
  \______  / ____| |___  /\___  >__|  /____  >\___  >\___  >____/ |__|  |__||__|  / ____|  |______  /\____/|__|  
         \/\/          \/     \/           \/     \/     \/                       \/              \/             
 ");
+
+using SoundPlayer player = new SoundPlayer("Startup.wav");
+
+
+player.Play();
+
+
+Console.WriteLine("Application started!");
+Console.ReadLine();

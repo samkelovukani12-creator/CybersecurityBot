07a52fc205d3c5093583f0cae63d8e6532f76222
Student <Student@LabVM2049939>
2026-08-24 13:52:41 +0200

    Added color to the application

--- CybersecurityBot/Program.cs
+++ CybersecurityBot/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Media;
-
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine(@"_________        ___.                                                  .__  __           __________        __   
 \_   ___ \___.__.\_ |__   ___________  ______ ____   ____  __ _________|__|/  |_ ___.__. \______   \ _____/  |_ 
 /    \  \<   |  | | __ \_/ __ \_  __ \/  ___// __ \_/ ___\|  |  \_  __ \  \   __<   |  |  |    |  _//  _ \   __\
@@ -11,9 +11,9 @@ Console.WriteLine(@"_________        ___.                                       
 
 using SoundPlayer player = new SoundPlayer("Startup.wav");
 
-
 player.Play();
 
-
 Console.WriteLine("Application started!");
-Console.ReadLine();
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}! Welcome to the Cybersecurity Chatbot.");

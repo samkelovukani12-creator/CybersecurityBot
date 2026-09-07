7a330ced526d5dfc8efcce735d294466ba80a765
Student <Student@LabVM2049939>
2026-09-07 14:24:55 +0200

    Added a user class

---
+++ CybersecurityBot/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    internal class User
+    {
+        // Attribute
+        protected string name;
+
+        // Constructor
+        public User(string name)
+        {
+            this.name = name;
+        }
+
+        // Method to get the user's name
+        public string GetName()
+        {
+            return name;
+        }
+
+        // Method to display the user
+        public void DisplayUser()
+        {
+            Console.WriteLine($"User: {name}");
+        }
+    }
+
